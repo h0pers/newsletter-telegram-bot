c03852f0302be324ed9102684ad982408b13c71f
Dmytro Hryshchenko <zzzxxxxx28540@gmail.com>
2024-02-18 20:48:48 +0000

    fix periodic schedule problem

--- schedule_tasks/app/main.py
+++ schedule_tasks/app/main.py
@@ -1,4 +1,13 @@
+from sqlalchemy import text
+
+from app.database.main import SessionLocal
+
+
+def refresh_periodic_messages_datetime():
+    with SessionLocal.begin() as session:
+        statement = 'UPDATE bot_periodic_message_tasks SET last_publish_time=NOW()'
+        session.execute(statement=text(statement))
 
 
 def run_schedule():
-    pass
+    refresh_periodic_messages_datetime()
