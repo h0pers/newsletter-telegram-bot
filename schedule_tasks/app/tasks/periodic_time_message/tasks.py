9599bf0151a82b7679306acad67295e3263d0e79
Dmytro Hryshchenko <zzzxxxxx28540@gmail.com>
2024-02-18 20:04:34 +0000

    add tasks schedule delay

--- schedule_tasks/app/tasks/periodic_time_message/tasks.py
+++ schedule_tasks/app/tasks/periodic_time_message/tasks.py
@@ -1,9 +1,8 @@
-import logging
-
+import random
 import telebot
+import datetime
 
 from sqlalchemy import select, text
-from datetime import datetime
 
 from app.celery import app, logger
 from app.config import BOT_TOKEN
@@ -15,28 +14,40 @@ from app.config import TIME_ZONE
 from app.database.models.tasks import BotPeriodicMessageTasks
 
 
+@app.task()
+def send_periodic_time_message(task):
+    bot = telebot.TeleBot(BOT_TOKEN, parse_mode='HTML')
+    try:
+        with SessionLocal.begin() as session:
+            statement = f'UPDATE bot_periodic_message_tasks SET last_publish_time=NOW() WHERE id={task["id"]};'
+            session.execute(statement=text(statement))
+            reply_chat = session.scalars(select(Channels).filter_by(id=task['reply_chat_id'])).one()
+
+        bot.forward_message(chat_id=reply_chat.chat_id, message_id=task['message_id'],
+                            message_thread_id=task['message_thread_id'], from_chat_id=task['from_chat_id'])
+    except Exception as error:
+        for admin_id in ADMINS_ID:
+            bot.send_message(chat_id=admin_id,
+                             text=MessageText.SEND_MESSAGE_ERROR.format(error=error))
+
+
 @app.task(bind=True)
 def check_periodic_time_message(self):
-    bot = telebot.TeleBot(BOT_TOKEN, parse_mode='HTML')
     with SessionLocal.begin() as session:
         tasks = session.scalars(select(BotPeriodicMessageTasks)).all()
 
     for task in tasks:
-        try:
-            time_now = datetime.now(tz=TIME_ZONE).now()
-            time_now = time_now.replace(second=0, microsecond=0)
-            last_publish_time = task.last_publish_time.replace(second=0, microsecond=0)
-
-            if time_now == last_publish_time + task.time_interval:
-                with SessionLocal.begin() as session:
-                    statement = f'UPDATE bot_periodic_message_tasks SET last_publish_time=NOW() WHERE id={task.id};'
-                    logging.info(statement)
-                    session.execute(statement=text(statement))
-                    reply_chat = session.scalars(select(Channels).filter_by(id=task.reply_chat_id)).one()
-
-                bot.forward_message(chat_id=reply_chat.chat_id, message_id=task.message_id,
-                                    message_thread_id=task.message_thread_id, from_chat_id=task.from_chat_id)
-        except Exception as error:
-            for admin_id in ADMINS_ID:
-                bot.send_message(chat_id=admin_id,
-                                 text=MessageText.SEND_MESSAGE_ERROR.format(error=error))
+        time_now = datetime.datetime.now(tz=TIME_ZONE).now()
+        time_now = time_now.replace(second=0, microsecond=0)
+        last_publish_time = task.last_publish_time.replace(second=0, microsecond=0)
+        if time_now == last_publish_time + task.time_interval:
+            task_kwargs = {
+                'id': task.id,
+                'reply_chat_id': task.reply_chat_id,
+                'message_id': task.message_id,
+                'message_thread_id': task.message_thread_id,
+                'from_chat_id': task.from_chat_id
+            }
+            time_now = datetime.datetime.now(tz=TIME_ZONE)
+            time_delay = datetime.timedelta(minutes=0, seconds=random.uniform(0, 5), microseconds=0)
+            send_periodic_time_message.apply_async(args=[task_kwargs], eta=time_now + time_delay)
--- schedule_tasks/app/tasks/static_time_message/tasks.py
+++ schedule_tasks/app/tasks/static_time_message/tasks.py
@@ -1,37 +1,60 @@
+import logging
+import random
+import datetime
 import telebot
 
-from sqlalchemy import select
-from datetime import datetime
+from sqlalchemy import text
 
 from app.celery import app, logger
 from app.config import BOT_TOKEN
 from app.database.main import SessionLocal
-from app.database.models.tasks import BotMessageTasks
-from app.database.models.channels import Channels
 from app.config import ADMINS_ID
 from app.config import MessageText
 from app.config import TIME_ZONE
 
 
+@app.task()
+def send_static_time_message(task):
+    bot = telebot.TeleBot(BOT_TOKEN, parse_mode='HTML')
+    try:
+        with SessionLocal.begin() as session:
+            statement = f"SELECT chat_id FROM channels WHERE id={task['reply_chat_id']};"
+            reply_chat = session.execute(text(statement))
+            reply_chat_id = reply_chat.scalar()
+
+        bot.forward_message(chat_id=reply_chat_id, message_id=task['message_id'],
+                            message_thread_id=task['message_thread_id'], from_chat_id=task['from_chat_id'])
+    except Exception as error:
+        for admin_id in ADMINS_ID:
+            bot.send_message(chat_id=admin_id,
+                             text=MessageText.SEND_MESSAGE_ERROR.format(error=error))
+
+
 @app.task(bind=True)
 def check_static_time_message(self):
-    bot = telebot.TeleBot(BOT_TOKEN, parse_mode='HTML')
     with SessionLocal.begin() as session:
-        tasks = session.scalars(select(BotMessageTasks)).all()
+        statement = f'''
+        SELECT (id, reply_chat_id, message_id, message_thread_id, from_chat_id, publish_time) 
+        FROM bot_message_tasks;
+        '''
+        tasks = session.scalars(statement=text(statement)).all()
 
     for task in tasks:
-        try:
-            time_now = datetime.now(tz=TIME_ZONE).time()
-            time_now = time_now.replace(second=0, microsecond=0)
-            task_publish_time = task.publish_time.replace(second=0, microsecond=0)
+        time_now = datetime.datetime.now(tz=TIME_ZONE).time()
+        time_now = time_now.replace(second=0, microsecond=0)
+        task_publish_time = datetime.datetime.strptime(task[5], '%H:%M:%S').replace(second=0, microsecond=0).time()
 
-            if task_publish_time == time_now:
-                with SessionLocal.begin() as session:
-                    reply_chat = session.scalars(select(Channels).filter_by(id=task.reply_chat_id)).one()
+        if task_publish_time == time_now:
+            task_kwargs = {
+                'id': task[0],
+                'reply_chat_id': task[1],
+                'message_id': task[2],
+                'message_thread_id': task[3],
+                'from_chat_id': task[4]
+            }
+            time_now = datetime.datetime.now(tz=TIME_ZONE)
+            time_delay = datetime.timedelta(minutes=0, seconds=random.uniform(0, 5), microseconds=0)
+            send_static_time_message.apply_async(args=[task_kwargs], eta=time_now + time_delay)
 
-                bot.forward_message(chat_id=reply_chat.chat_id, message_id=task.message_id,
-                                    message_thread_id=task.message_thread_id, from_chat_id=task.from_chat_id)
-        except Exception as error:
-            for admin_id in ADMINS_ID:
-                bot.send_message(chat_id=admin_id,
-                                 text=MessageText.SEND_MESSAGE_ERROR.format(error=error))
+
+
